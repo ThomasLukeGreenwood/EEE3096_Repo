5e32a880e29d20eddfdf3eaa6858a861fabead48
Thomas Greenwood <grntho006@myuct.ac.za>
2023-08-04 10:47:44 +0200

    Added the a.out file

--- 3096-Pracs-GRNTHO006/Prac1/main.c
+++ 3096-Pracs-GRNTHO006/Prac1/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(){
-	int a, b , sum;
+	int a, b, sum;
 
-	printf("Enter a value for a:");
+	printf("Enter a value for a: ");
 	scanf("%d", &a);
 
-	printf("Enter a value for b:");
-	scanf("%d", &b);
+	printf("Enter a value for b: ");
+	scanf("%d",&b);
 
 	sum = a + b;
 
